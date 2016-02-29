df0b9f7a5320945c23c7a98668ef3193dd207d56
gleidsonmendes <gmendes.costa13@gmail.com>
2016-02-29 09:36:26 -0300

    Versao final para as estatisticas e aeroporto

--- AeroportSim.py
+++ AeroportSim.py
@@ -4,10 +4,10 @@ import simpy
 
 
 RANDOM_SEED = 42
-NUM_GATES = 2  # Number of machines in the carwash
-WAIT_TIME = 5      # Minutes it takes to clean a car
-T_INTER = 7       # Create a car every ~7 minutes
-SIM_TIME = 200    # Simulation time in minutes
+NUM_GATES = 2
+WAIT_TIME = 5
+T_INTER = 7
+SIM_TIME = 200
 
 usage = False
 
@@ -21,18 +21,18 @@ class Airport(object):
 
     def leave(self, plane):
         yield self.env.timeout(WAIT_TIME)
-        print("Airport authorizes %s to leave at %d" % (plane, env.now))
+        #print("Airport authorizes %s to leave at %d" % (plane, env.now))
 
 
 def plane(env, name, ap):
     #print("tempo sem locacao: u%d   nu%d    t%d" % (usage_res, non_usage_res, env.now))
 
-    print('%s arrives at the airport at %.2f.' % (name, env.now))
+    #print('%s arrives at the airport at %.2f.' % (name, env.now))
     with ap.num_gates.request() as request:
         yield request
 
-        arrival_time = env.now
-        print('%s enters the gate at %.2f.' % (name, arrival_time))
+        #arrival_time = env.now
+        #print('%s enters the gate at %.2f.' % (name, arrival_time))
         yield env.process(ap.leave(name))
         stats.addCompletions()
 
@@ -41,42 +41,36 @@ def plane(env, name, ap):
         else:
             ap.open = False
 
-        depart_time = env.now
-        print('%s leaves the gate at %.2f.' % (name, depart_time))
+        #depart_time = env.now
+        #print('%s leaves the gate at %.2f.' % (name, depart_time))
 
 
 def setup(env, num_gates, wait_time, t_inter):
-    # Create the airport
     airport = Airport(env, num_gates, wait_time)
-    # Create 4 initial planes
     for i in range(1):
         env.process(plane(env, 'Plane %d' % i, airport))
         stats.addArrivals()
 
     usage_res = env.now
-    # Create more cars while the simulation is running
     while True:
         yield env.timeout(random.randint(t_inter-2, t_inter+2))
         i += 1
         env.process(plane(env, 'Plane %d' % i, airport))
         stats.addArrivals()
 
-        print(airport.num_gates.count)
+        #print(airport.num_gates.count)
         if not airport.open:
             stats.addBusyTime(env.now - usage_res)
             usage_res = env.now
 
 stats = Statistics(SIM_TIME)
 
-# Setup and start the simulation
-print('Airport')
-random.seed(RANDOM_SEED)  # This helps reproducing the results
+#print('Airport')
+random.seed(RANDOM_SEED)
 
-# Create an environment and start the setup process
 env = simpy.Environment()
 env.process(setup(env, NUM_GATES, WAIT_TIME, T_INTER))
 
-# Execute!
 env.run(until=SIM_TIME)
 
-stats.printStats()
+stats.printStats("Airport")
--- Statistics.py
+++ Statistics.py
@@ -5,13 +5,17 @@ class Statistics(object):
     completions_num = 0
     busy_time = 0
 
+    num_recursos = 0
+
+
     def __init__(self, observation_time):
         self.observation_time = observation_time
         self.arrivals_num = 0
         self.completions_num = 0
         self.busy_time = 0
+        self.num_recursos = 0
 
-    def __setObservationTime(self, t):
+    def setObservationTime(self, t):
         self.observation_time = t
 
     def addArrivals(self):
@@ -22,6 +26,9 @@ class Statistics(object):
 
     def addBusyTime(self, t):
         self.busy_time += t
+
+    def setNumRecursos(self, t):
+        self.num_recursos = t
 
     def arrivalRate(self):
         return self.arrivals_num/self.observation_time
@@ -36,13 +43,17 @@ class Statistics(object):
         return self.busy_time/self.completions_num
 
 
-    def printStats(self):
-        print("Tempo de Observacao %d" % (self.observation_time))
-        print("Numero de Chegadas %d" % self.arrivals_num)
-        print("Numero de Saidas %d " % self.completions_num)
-        print("Tempo ocupado %.2f" % self.busy_time)
+    def printStats(self, titulo):
+        print("---------------%s Statistics -----------------------" % titulo)
+        print(" Numero de Recursos: %d" % self.num_recursos)
 
-        print("Taxa de chegada: %.2f" % self.arrivalRate())
-        print("Taxa de saida: %.2f" % self.throughput())
-        print("Utilizacao: %.2f" % self.usage())
-        print("Tempo Medio Servico: %.2f" % self.avgServiceTime())
+        print("Tempo de Observacao: ......%d" % self.observation_time)
+        print("Numero de Chegadas:  ......%d" % self.arrivals_num)
+        print("Numero de Saidas:    ......%d " % self.completions_num)
+        print("Tempo Recursos Ocupados: ..%.2f" % self.busy_time)
+
+        print("Taxa de chegada: ..........%.2f" % self.arrivalRate())
+        print("Taxa de saida:   ..........%.2f" % self.throughput())
+        print("Utilizacao:  ..............%.2f%%" % (self.usage()*100))
+        print("Tempo Medio Servico: ......%.2f" % self.avgServiceTime())
+
