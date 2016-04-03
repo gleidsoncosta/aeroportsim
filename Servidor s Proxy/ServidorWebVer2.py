360f974f58261bcbf6d4600c6057efab69fc705d
gleidsonmendes <gmendes.costa13@gmail.com>
2016-04-03 02:02:36 -0300

    Requisicao [e enviada agrupada

--- Servidor s Proxy/ServidorWebVer2.py
+++ Servidor s Proxy/ServidorWebVer2.py
@@ -16,7 +16,7 @@ TaxaDadosNet = 2.5                                      # 20Kbps == 2.5 Kbyte/s
 TaxaBrowser = 0.3                                       #num pedidos por segundo
 
 NumMicros = 150                                         #num micros na rede
-PorcMicrosUsage = 10                                    #porcentagem de micros ativos
+PorcMicrosUsage = 0.1                                    #porcentagem de micros ativos
 NumMicrosActive = (NumMicros/100) * PorcMicrosUsage     #num micros ativos na rede
 
 PedidoHTTPMedio = 290                                   #bytes, tamanho medio da requisicao http para servidor
@@ -40,6 +40,12 @@ class Pct(object):
         self.numPartsTotal = numPartsTotal
         self.sizePct = sizePct
 
+class Requisicao(object):
+    def __init__(self, requisicaoID, size, complete):
+        self.requisicaoID = requisicaoID
+        self.size = size
+        self.complete = complete
+
 class LinkSai(object):
     def __init__(self, env, rttAtrasoNet, taxaDadosNet):
         self.env = env
@@ -47,18 +53,36 @@ class LinkSai(object):
         self.taxaDadosNet = taxaDadosNet
         self.conexao = simpy.Resource(env, 1)
 
+        self.req_list = []
+
     def takePct(self, pacote):
-        time_to_pass = int(pacote.sizePct/self.taxaDadosNet) + 1
+        found = False
+        for req in self.req_list:
+            if pacote.requisicaoID == req.requisicaoID:
+                found = True
+                req.size = req.size + pacote.sizePct - OvhdFrame
+                if pacote.partePCT == (pacote.numPartsTotal):
+                    req.complete = True
 
-        for i in range(0, time_to_pass):
-            yield self.env.timeout(self.rttAtrasoNet)
-            print('Requisição: %s Pacote: %s tamPct: %s saiu do roteador: %.2f' %
-                  (pacote.requisicaoID, pacote.partePCT, pacote.sizePct, env.now))
+            break
 
+        if not found:
+            complete = False
+            if pacote.partePCT == (pacote.numPartsTotal):
+                complete = True
+            self.req_list.append(Requisicao(pacote.requisicaoID, pacote.sizePct, complete))
 
+        for req in self.req_list:
+            print(req.complete)
+            if req.complete:
+                time_to_pass = int(req.size/self.taxaDadosNet) + 1
+                for i in range(0, time_to_pass):
+                    yield self.env.timeout(self.rttAtrasoNet)
+                    print('Requisição: %s tamreq: %s saiu do roteador: %.2f' %
+                          (req.requisicaoID, req.size, env.now))
 
 class Router (object):
-    def __init__(self, env, numMicros, taxa_browser, latencia_rout, mss, ovh_frame):
+    def __init__(self, env, numMicros, taxa_browser, latencia_rout, mss, ovh_frame, largura_banda_link):
         self.env = env
 
         #aqui ele pega o numero de micros na minha rede, somente
@@ -78,6 +102,8 @@ class Router (object):
         #botei inicial somente 1
         self.conexao = simpy.Resource(env, 1)
 
+        self.largura_banda_link = largura_banda_link
+
     def makeAPctNavServer(self, requisicaoID, sizeDoc):
         #calcular quantos datagramas esta requisicao esta consumindo
         #inicialmente 1 requisicao = datagrama
@@ -93,7 +119,9 @@ class Router (object):
         #ate o envio de todos
         for i in range(0, num_pacs):
             yield self.env.timeout(self.latencia_rout)
-            Pct_list.append(Pct(requisicaoID, i, num_pacs, self.mss+self.ovh_frame))
+            yield self.env.timeout((self.mss/1024)/(self.largura_banda_link))
+            Pct_list.append(Pct(requisicaoID, i, num_pacs-1, self.mss+self.ovh_frame))
+            print("num pack: %s, num packs: %s" % (i, num_pacs-1))
             print('Requisição: %s Pacote: %s saiu do roteador: %.2f' % (requisicaoID, i, env.now))
 
     def makeAPctServerNav(self, requisicaoID, sizeDoc):
@@ -111,7 +139,9 @@ class Router (object):
         #ate o envio de todos
         for i in range(0, num_pacs):
             yield self.env.timeout(self.latencia_rout)
-            Pct_list.append(Pct(requisicaoID, i, num_pacs, self.mss+self.ovh_frame))
+            yield self.env.timeout((self.mss/1024)/(self.largura_banda_link))
+            Pct_list.append(Pct(requisicaoID, i, num_pacs-1, self.mss+self.ovh_frame))
+            print("num pack: %s, num packs: %s" % (i, num_pacs-1))
             print('Requisição: %s Pacote: %s saiu do roteador: %.2f' % (requisicaoID, i, env.now))
 
 def requests (env, router, requestID, sizeDoc):
@@ -124,16 +154,16 @@ def requests (env, router, requestID, sizeDoc):
         print('Requisição: %s foi atendida no tempo: %.2f' %(requestID, env.now))
 
 def requestLinkSai(env, linksai, pacote):
+
     with linksai.conexao.request() as request:
         yield request
 
-            #ficara nisso ate todos os pacotes serem enviados
+        #ficara nisso ate todos os pacotes serem enviados
         yield env.process(linksai.takePct(pacote))
-            #print('Requisição: %s foi atendida no tempo: %.2f' %(requestID, env.now))
 
 def setup (env):
     #Criando o Roteador
-    router = Router(env, NumMicrosActive, TaxaBrowser, RouterLatencia, MSS, OvhdFrame)
+    router = Router(env, NumMicrosActive, TaxaBrowser, RouterLatencia, MSS, OvhdFrame, LargBandaLink)
     linkSai = LinkSai(env, RttAtrasoNet, TaxaDadosNet)
 
     #primeiro id de requisição
